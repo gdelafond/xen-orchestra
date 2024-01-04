e095858793c07b63cf6511164ace0a2fdde93a88
Julien Fontanet <julien.fontanet@isonoe.net>
2024-01-04 17:35:40 +0100

    add XO tasks

--- @xen-orchestra/xapi/pool.mjs
+++ @xen-orchestra/xapi/pool.mjs
@@ -1,5 +1,6 @@
 import { asyncEach } from '@vates/async-each'
 import { createLogger } from '@xen-orchestra/log'
+import { Task } from '@vates/task'
 
 import { getCurrentVmUuid } from './_XenStore.mjs'
 
@@ -29,13 +30,30 @@ export default class Pool {
       // try to move current VM on pool master
       const hostRef = await this.call('VM.get_resident_on', currentVmRef)
       if (hostRef !== poolMasterRef) {
-        await this.callAsync('VM.pool_migrate', currentVmRef, poolMasterRef, {})
+        await Task.run(
+          {
+            properties: {
+              name: 'Migrating current VM to pool master',
+            },
+          },
+          () => this.callAsync('VM.pool_migrate', currentVmRef, poolMasterRef, {})
+        ).catch(noop)
       }
     } catch (error) {
       warn(error)
     }
 
-    await pCatch(this.call('pool.disable_ha'), 'HA_NOT_ENABLED', noop).catch(warn)
+    await pCatch(this.call('pool.disable_ha'), 'HA_NOT_ENABLED', noop)
+
+    const shutdownHost = ref =>
+      Task.run(
+        {
+          properties: {
+            name: 'shutting down host',
+          },
+        },
+        () => this.callAsync('host.shutdown', ref)
+      ).catch(noop)
 
     const hostRefs = await this.call('host.get_all')
     await asyncEach(hostRefs, async hostRef => {
@@ -45,16 +63,23 @@ export default class Pool {
       await asyncEach(vmRefs, vmRef => {
         // never stop current VM otherwise the emergencyShutdown process would be interrupted
         if (vmRef === currentVmRef) {
-          return pCatch(this.callAsync('VM.suspend', vmRef), 'VM_BAD_POWER_STATE', noop).catch(warn)
+          return Task.run(
+            {
+              properties: {
+                name: 'suspending VM',
+              },
+            },
+            () => pCatch(this.callAsync('VM.suspend', vmRef), 'VM_BAD_POWER_STATE', noop)
+          ).catch(noop)
         }
       })
 
       // pool master will be shutdown at the end
       if (hostRef !== poolMasterRef) {
-        await this.callAsync('host.shutdown', hostRef).catch(warn)
+        await shutdownHost(hostRef)
       }
     })
 
-    await this.callAsync('host.shutdown', poolMasterRef)
+    await shutdownHost(poolMasterRef)
   }
 }
